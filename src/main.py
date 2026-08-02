2819a3e8aa27c88e2a9da9c2cac508929194945e
yash-yb <yashbhanushali452007@gmail.com>
2026-08-02 14:06:45 +0530

    added image capture to use for correctly knowing whos face it is

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import capture_frame
 
 cap=cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-
+face_captured=0
 
 if not cap.isOpened():
     print("Cannot open camera")
@@ -15,12 +15,15 @@ while True:
         print("Can't receive frame (stream end?). Exiting ...")
         break
 
-    
     frame=cv2.flip(frame, 1)
 
     
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=4)
+    if(face_captured==0):
+        if(faces is not None):
+            cv2.imwrite(r'Recognize_face_data/captured_frame.jpg', frame)
+            face_captured=1
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
         '''face_roi = frame[y:y + h, x:x + w]
